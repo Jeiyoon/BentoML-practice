bf6ad511135b24d3830ed8239aef4f41909cff1b
Your Name <k4ke@korea.ac.kr>
2023-03-07 14:52:59 +0900

    main and classifier

--- iris_classifier.py
+++ iris_classifier.py
@@ -1,3 +1,29 @@
 import pandas as pd
 
 from bentoml import env, artifacts, api, BentoService
+from bentoml.adapters import DataframeInput
+from bentoml.frameworks.sklearn import SklearnModelArtifact
+
+"""
+1. @env 데코레이터
+2. @artifacts 데코레이터
+    - BentoML에서 미리 만든 artifacts를 사용
+    - 여기서 model은 prediction Service Class에서 부를 이름
+    - predict 함수에서 self.artifacts.model.predict가 있는데 이 model을 의미함
+"""
+@env(infer_pip_packages=True)
+@artifacts([SklearnModelArtifact("model")])
+class IrisClassifier(BentoService):
+    """
+    A minimum prediction service exposing a Scikit-learn model
+    """
+    # 데코레이터 설정시 추후 inference API로 만들어줌
+    # API의 input, output 설정. batch 유무를 인자로 받을 수 있음
+    @api(input=DataframeInput(), batch=True)
+    def predict(self, df: pd.DataFrame):
+        """
+        An inference API named `predict` with Dataframe input adapter, which codifies
+        how HTTP requests or CSV files are converted to a pandas Dataframe object as the
+        inference API function input
+        """
+        return self.artifacts.model.predict(df)
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from sklearn import svm
 from sklearn import datasets
 
+from iris_classifier import IrisClassifier
+
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
 
 clf = svm.SVC(gamma='scale')
 clf.fit(X, y)
 
-print("Done")
+print("Training Done.")
+
+# Create a iris classifier service instance
+iris_classifier_service = IrisClassifier()
+
+# Pack the newly trained model artifact
+# 생성한 모델인 clf를 iris_classifier_service에 pack함 (모델 주입)
+iris_classifier_service.pack('model', clf)
+
+# Save the prediction service to disk for model serving
+# Prediction Serevice를 BENTOML_HOME에 저장하는 코드
+save_path = iris_classifier_service.save()
