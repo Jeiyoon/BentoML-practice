c35c57f94ef248d6f5a424801561580cc608daed
Your Name <k4ke@korea.ac.kr>
2023-03-07 16:22:42 +0900

    test

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import bentoml.sklearn
 from sklearn import svm
 from sklearn import datasets
 
-from iris_classifier import IrisClassifier
+# from iris_classifier import IrisClassifier
 
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
@@ -9,15 +10,19 @@ X, y = iris.data, iris.target
 clf = svm.SVC(gamma='scale')
 clf.fit(X, y)
 
-print("Training Done.")
+print(f"Model trained.")
+
+saved_model = bentoml.sklearn.save_model("iris_clf", clf)
+
+print(f"Model saved: {saved_model}")
 
 # Create a iris classifier service instance
-iris_classifier_service = IrisClassifier()
+# iris_classifier_service = IrisClassifier()
 
 # Pack the newly trained model artifact
 # 생성한 모델인 clf를 iris_classifier_service에 pack함 (모델 주입)
-iris_classifier_service.pack('model', clf)
+# iris_classifier_service.pack('model', clf)
 
 # Save the prediction service to disk for model serving
 # Prediction Serevice를 BENTOML_HOME에 저장하는 코드
-save_path = iris_classifier_service.save()
+# save_path = iris_classifier_service.save()
